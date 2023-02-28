20f5b4f0d4c4d6cffd0215ae1a3ca6e52c9f1a60
nouri50 <n.morouche@gmail.com>
2023-02-28 15:52:32 +0100

    exo js

---
+++ exo1/script.js
@@ -0,0 +1,16 @@
+let age = prompt ('age')
+let sexe = prompt('sexe')
+
+let casHomme = (age>20 && sexe ==='M')
+let casFemme = (age > 18 && age < 35 && sexe === 'F')
+let  imposable = cas
+
+console.log("double egale avec type diff : ", 2=='2');
+console.log("double egale avec type diff : ", 2==2);
+console.log("double egale avec type diff : ", 2=='2');
+
+if (age > 20 && sexe === 'M')|| (age> 18 && age < 35 && sexe ==='F'){
+    Console.log ('vous etes imposable')
+
+} else
+console.log ('vous etes non imposable')

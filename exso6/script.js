188e1f042169e456e6ada79e0c77fc032d578747
nouri50 <n.morouche@gmail.com>
2023-03-01 15:49:39 +0100

    deleted:    exso5/exso6/index.html 	deleted:    exso5/exso6/script.js 	modified:   exso6/index.html 	modified:   exso6/script.js

--- exso6/script.js
+++ exso6/script.js
@@ -1,3 +1,19 @@
-let=1
-let-100
 
+let nombreAleatoire = Math.floor(Math.random()* 100) + 1;
+let tentatives = 10;
+
+for (let i = 0; i < tentatives; i++) {
+    let essai = prompt("Entrez votre essai : ");
+    if (essai == nombreAleatoire) {
+        alert("Bravo ! Vous avez deviné le nombre en " + (i+1) + " tentatives.");
+        break;
+    } else if (essai < nombreAleatoire) {
+        alert("Le nombre est plus grand.");
+    } else {
+        alert("Le nombre est plus petit.");
+    }
+}
+
+if (essai != nombreAleatoire) {
+    alert("Désolé, vous n'avez pas réussi à deviner le nombre. Le nombre était " + nombreAleatoire);
+}

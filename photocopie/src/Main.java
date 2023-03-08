dfe6405f29f1bf0b93cb95dfac0071a279ababcd
unknown <n.morouche@gmail.com>
2023-03-08 12:29:45 +0100

    exso java

---
+++ photocopie/src/Main.java
@@ -0,0 +1,14 @@
+import photocie.scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int bnbillet10 =0;
+        int nbrbillet5 = 0;
+        int nbbpiece = 0;
+        int reste = 0 ;
+
+
+        scanner sc = new scanner(sytem  .in )
+
+    }
+}
